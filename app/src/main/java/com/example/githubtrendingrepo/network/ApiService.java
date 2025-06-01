5d1533642ee5fe92229320b72adeb9ab4368ad35
Abdelali-Moutawassit <abdelalimoutawassit99@gmail.com>
2025-06-01 19:00:39 +0100

    API

--- app/src/main/java/com/example/githubtrendingrepo/network/ApiService.java
+++ app/src/main/java/com/example/githubtrendingrepo/network/ApiService.java
@@ -1,4 +1,18 @@
 package com.example.githubtrendingrepo.network;
 
-public class ApiService {
+import com.example.githubtrendingrepo.model.GitHubResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET("search/repositories")
+    Call<GitHubResponse> getTrendingRepositories(
+            @Query("q") String createdQuery,
+            @Query("sort") String sortBy,
+            @Query("order") String order,
+            @Query("per_page") int perPage
+    );
 }
